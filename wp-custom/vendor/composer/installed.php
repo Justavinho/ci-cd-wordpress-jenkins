46c896c5fab19fcb8e365a1816e6dcac38509dcb
GOAT JUSTAV <joshua.diaz@utp.ac.pa>
2025-12-03 22:15:30 +0000

    Ajustar configuración de PHPStan y dependencias de análisis

--- wp-custom/vendor/composer/installed.php
+++ wp-custom/vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'estudiante/wp-custom-analysis',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => null,
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'ce71f668040d9a29e6193a0a0b5f99f3787d863b',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         'estudiante/wp-custom-analysis' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => null,
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'ce71f668040d9a29e6193a0a0b5f99f3787d863b',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
